53ab687ef552fd827ef76cd3a4994555e0b21cf9
JNC96 <jncho3@student.monash.edu>
2019-09-17 15:45:42 +1000

    Update __init__.py

--- gym_drone/__init__.py
+++ gym_drone/__init__.py
@@ -3,4 +3,8 @@ from gym.envs.registration import register
 register(
     id='drone-v0',
     entry_point='gym_drone.envs:DroneEnv',
+    max_episode_steps=50,
+    reward_threshold = 0.95,
+    nondeterministic = False
 )
+
