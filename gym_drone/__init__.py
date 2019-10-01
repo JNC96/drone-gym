1a18dae5b6d71eef27afaefc98fd70bcb4e452b7
JNC96 <jncho3@student.monash.edu>
2019-10-01 20:46:23 +1000

    Added interactive environment

--- gym_drone/__init__.py
+++ gym_drone/__init__.py
@@ -8,3 +8,11 @@ register(
     nondeterministic = False
 )
 
+register(
+    id='droneInt-v0',
+    entry_point='gym_drone.envs:DroneIntEnv',
+    max_episode_steps=50,
+    reward_threshold=0.95,
+    nondeterministic = False,
+)
+
