43c2a0553ac0f0f960cb4e61b88f3b20a77feb09
JNC96 <jncho3@student.monash.edu>
2019-10-21 22:27:37 +1100

    Update report_tester_mayhem.py

--- report_tester_mayhem.py
+++ report_tester_mayhem.py
@@ -1,10 +1,26 @@
-if self.agent.episodes == 0 and self.interactive:
-                print(self.interactive)
-                print(self.agent.episodes)
-                user_action = self.action_rank(states=states, evaluation=evaluation)
-            else:
-                user_action = 0
-            # run with selected action
-            if self.agent.episodes > 0:
-                self.interactive = False
-            actions = self.agent.act(states=states, evaluation=evaluation, int_bool = self.interactive, int_act = user_action)
+agent = Agent.create(
+        agent='ppo', environment=env,
+        # Automatically configured network
+        network='auto',
+        # Optimization
+        batch_size=10, update_frequency=2, learning_rate=1e-3, subsampling_fraction=0.2,
+        optimization_steps=5,
+        # Reward estimation
+        likelihood_ratio_clipping=0.2, discount=0.99, estimate_terminal=False,
+        # Critic
+        critic_network='auto',
+        critic_optimizer=dict(optimizer='adam', multi_step=10, learning_rate=1e-3),
+        # Preprocessing
+        preprocessing=None,
+        # Exploration
+        exploration=0.0, variable_noise=0.0,
+        # Regularization
+        l2_regularization=0.0, entropy_regularization=0.0,
+        # TensorFlow etc
+        name='agent', device=None, parallel_interactions=1, seed=None, execution=None, saver=None,
+        summarizer=dict(
+            directory="D:\summ",
+            labels="all"
+            ),
+        recorder=None
+    )
